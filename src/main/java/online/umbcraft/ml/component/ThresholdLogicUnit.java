d82c222ef9f2efd1bda227b1e0475230bb2349d3
Ivan8or <ivanthegreat825@gmail.com>
2021-12-20 13:34:03 -0500

    added method to get / set weights of a tlu

--- src/main/java/online/umbcraft/ml/component/ThresholdLogicUnit.java
+++ src/main/java/online/umbcraft/ml/component/ThresholdLogicUnit.java
@@ -1,7 +1,6 @@
 package online.umbcraft.ml.component;
 
 import online.umbcraft.ml.activations.ActivationFunction;
-import online.umbcraft.ml.component.Connection;
 
 import java.util.List;
 
@@ -16,6 +15,24 @@ public class ThresholdLogicUnit {
         this.activationFunction = activationFunction;
     }
 
+    // returns a list of all the connection weights as an array of values
+    public double[] getWeights() {
+        double[] toReturn = new double[inputs.size()];
+        for(int i = 0; i < inputs.size(); i++) {
+            toReturn[i] = inputs.get(i).getWeight();
+        }
+        return toReturn;
+    }
+
+    // sets the weights for all connections according to an array of weight values
+    public void setWeights(double[] weights) {
+        if(weights.length != inputs.size())
+            throw new IllegalArgumentException("invalid amount of weights!");
+
+        for(int i = 0; i < inputs.size(); i++) {
+            inputs.get(i).setWeight(weights[i]);
+        }
+    }
 
     public List<Connection> getInputs() {
         return inputs;
